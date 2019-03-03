1c82ab0cf89656f0515192e2bd761683695689af
Ville M. Vainio <ville.vainio@basware.com>
2019-03-03 13:45:36 +0200

    quickfilter

--- gridomancer.ts
+++ gridomancer.ts
@@ -1,27 +1,24 @@
 // from ag-grid
 declare var agGrid;
 
-
 class Gridomancer {
   el: Element;
-  filter: Element;
-
-
+  filter: HTMLInputElement;
+  grid: any;
+  gridOptions: { api; defaultColDef: { filter: boolean; sortable: boolean; resizable: boolean; }; columnDefs: any; rowData: any; };
 
   setEl(element: Element) {
     this.el = element;
     return this;
   }
 
-  setFilter(element: Element) {
+  setFilter(element: HTMLInputElement) {
     this.filter = element;
     element.addEventListener("input", ev => {
+      this.gridOptions.api.setQuickFilter(this.filter.value)
 
-
-
-    })
+    });
     return this;
-
   }
 
   mountDefaults() {
@@ -33,49 +30,49 @@ class Gridomancer {
   loadUrl(url: string) {
     fetch(url)
       .then(r => r.json())
-      .then(resp => formatResponse(resp, this.el));
+      .then(resp => {
+        this.grid = this.startWithData(resp, this.el);
+        console.log('grid is', this.grid)
+      });
   }
 
   loadByQueryParam(paramName: string) {
     const params = new URLSearchParams(document.location.search);
     const url = params.get(paramName);
     if (url) {
-        const decoded = decodeURIComponent(url);
-        this.loadUrl(decoded);
+      const decoded = decodeURIComponent(url);
+      this.loadUrl(decoded);
     }
+  }
+
+  private startWithData(resp: { Header; Rows }, element: Element) {
+    const headerArr = resp.Header;
+    const coldefs = headerArr.map(rh => ({
+      headerName: rh,
+      field: rh
+    }));
+
+    const rows = resp.Rows.map(row => {
+      const r = {};
+      for (let i = 0; i < headerArr.length; i++) {
+        r[headerArr[i]] = row[i];
+      }
+      return r;
+    });
+
+    this.gridOptions = {
+      defaultColDef: {
+        filter: true,
+        sortable: true,
+        resizable: true
+      },
+      columnDefs: coldefs,
+      rowData: rows
+    };
+    new agGrid.Grid(element, this.gridOptions);
   }
 
 }
 
-function formatResponse(resp: { Header; Rows;}, element: Element) {
-  console.log(resp);
-  const headerArr = resp.Header;
-  const coldefs = headerArr.map(rh => ({
-    headerName: rh,
-    field: rh
-  }));
-
-  const rows = resp.Rows.map(row => {
-    const r = {};
-    for (let i = 0; i < headerArr.length; i++) {
-      r[headerArr[i]] = row[i];
-    }
-    return r;
-  });
-
-  const gridOptions = {
-    defaultColDef: {
-      filter: true,
-      sortable: true,
-      resizable: true,
-
-    },
-    columnDefs: coldefs,
-    rowData: rows
-  };
-  //console.log('rows', rows)
-
-  new agGrid.Grid(element, gridOptions);
-}
 // exposed as global
-window['Gridomancer'] = Gridomancer;
+window["Gridomancer"] = Gridomancer;
